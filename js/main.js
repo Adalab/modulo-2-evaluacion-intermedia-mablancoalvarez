d506900ccf3096199c00d8e256a1f1bd8dbab1dd
mablancoalvarez <mablancoalvarez@gmail.com>
2020-03-24 14:33:26 +0100

    fix issues such us if and else estructure or new function in parseInt

--- js/main.js
+++ js/main.js
@@ -4,50 +4,42 @@ const button = document.querySelector('#button-try');
 const inputNumber = document.querySelector('#inputNumber');
 const clueText = document.querySelector('#clue');
 const attempsText = document.querySelector('#textAttemps');
-
-
 let attempts = 0;
 
- function getRandomNumber(max) {
+function getRandomNumber(max) {
    return Math.ceil(Math.random() * max);
-   }
-
+}
 const randomNumber = getRandomNumber(100);
 console.log(randomNumber)
 
 
 function showNumber() {
    let myNumber = inputNumber.value;
-console.log(myNumber);
+   console.log(myNumber);
 }
 
-function compareNumber () {
+function parseIntNumber(number) {
+   return parseInt(number);
+}
 
- 
+function compareNumber() {
+
    attempts += 1;
-  attempsText.innerHTML =`Número de intentos:${attempts}`;
- 
-   
-let myNumber = parseInt(inputNumber.value);
-
-
- if (myNumber > randomNumber ) {
-   clueText.innerHTML = 'Demasiado alto';
- }
-
- if (myNumber < randomNumber) {
-   clueText.innerHTML = 'Demasiado bajo';  
-}
- if (myNumber === randomNumber ){
-   clueText.innerHTML = 'Has ganado campeona!!!'; 
- }
-
- if (myNumber > 100 || myNumber < 0) {
-   clueText.innerHTML = 'El número debe estar entre 1 y 100'
-
-}
+   attempsText.innerHTML = `Número de intentos:${attempts}`;
+   let myNumber = parseIntNumber(inputNumber.value);
+   if (myNumber > randomNumber && myNumber < 100) {
+      clueText.innerHTML = 'Demasiado alto';
+   }
+   else if (myNumber < randomNumber && myNumber > 0) {
+      clueText.innerHTML = 'Demasiado bajo';
+   }
+   else if (myNumber === randomNumber) {
+      clueText.innerHTML = 'Has ganado campeona!!!';
+   }
+   else {
+      clueText.innerHTML = 'El número debe estar entre 1 y 100';
+   }
 }
 
-
-button.addEventListener('click',showNumber);
-button.addEventListener('click',compareNumber);
+button.addEventListener('click', showNumber);
+button.addEventListener('click', compareNumber);
